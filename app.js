b32c86d8f851ad35fc8e99e1895272342d31e585
saraarawr <marinkitagawa.x86@gmail.com>
2025-01-17 01:10:03 -0500

    initial commit
    Signed-off-by: saraarawr <marinkitagawa.x86@gmail.com>

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 
 const app = express();
-const PORT = 80;
+const PORT = 80; // Listen on port 80
 
-// Set EJS as templating engine 
+// Set EJS as templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -17,14 +17,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Proxy all non-homepage requests
-app.use('/*', (req, res, next) => {
-    // Skip proxying for homepage and static assets
-    if (req.path === '/' || req.path.startsWith('/public')) {
-        return next();
-    }
-
+// Proxy Endpoint
+app.get('/proxy', (req, res, next) => {
     const targetUrl = req.query.url;
+
     if (!targetUrl) {
         return res.status(400).send('Missing url parameter.');
     }
@@ -37,29 +33,20 @@ app.use('/*', (req, res, next) => {
         return res.status(400).send('Invalid URL.');
     }
 
-    // Create proxy middleware dynamically
-    const proxy = createProxyMiddleware({
-        target: parsedUrl.origin,
+
+    // Create Proxy Middleware
+    return createProxyMiddleware({
+        target: targetUrl,
         changeOrigin: true,
-        secure: false,
-        followRedirects: true,
-        pathRewrite: (path) => {
-            // Remove the /proxy prefix and url parameter
-            return parsedUrl.pathname + parsedUrl.search;
-        },
-        onProxyRes: (proxyRes, req, res) => {
-            // Modify response headers to handle CORS and security
-            proxyRes.headers['access-control-allow-origin'] = '*';
-            delete proxyRes.headers['x-frame-options'];
-            delete proxyRes.headers['content-security-policy'];
+        pathRewrite: {
+            '^/proxy': '',
         },
         onError: (err, req, res) => {
-            console.error('Proxy error:', err);
-            res.status(500).send('Error accessing the requested URL');
-        }
-    });
-
-    return proxy(req, res, next);
+            console.error(err);
+            res.status(500).send('Proxy error.');
+        },
+        logLevel: 'silent', // Change to 'debug' for verbose logs
+    })(req, res, next);
 });
 
 // Start the Server
