696bb87680266da9e713286d2cd17315b937ca33
saraarawr <marinkitagawa.x86@gmail.com>
2025-01-17 20:53:55 -0500

    initial commit
    Signed-off-by: saraarawr <marinkitagawa.x86@gmail.com>

--- app.js
+++ app.js
@@ -1,62 +1,89 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-// https://stackoverflow.com/a/63602976/470749
 const express = require('express');
-
-const app = express();
+const path = require('path');
 const https = require('https');
 const http = require('http');
-// const { response } = require('express');
 
-const targetUrl = process.env.TARGET_URL || 'https://google.com'; // Run localtunnel like `lt -s rscraper -p 8080 --print-requests`; then visit https://yourname.loca.lt/todos/1 .
+const app = express();
+const PORT = 3000; // Listen on port 3000
 
-const proxyServerPort = process.env.PROXY_SERVER_PORT || 3000;
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// eslint-disable-next-line max-lines-per-function
-app.use('/', function (clientRequest, clientResponse) {
-  const parsedHost = targetUrl.split('/').splice(2).splice(0, 1).join('/');
-  let parsedPort;
-  let parsedSSL;
-  if (targetUrl.startsWith('https://')) {
-    parsedPort = 443;
-    parsedSSL = https;
-  } else if (targetUrl.startsWith('http://')) {
-    parsedPort = 80;
-    parsedSSL = http;
-  }
-  const options = {
-    hostname: parsedHost,
-    port: parsedPort,
-    path: clientRequest.url,
-    method: clientRequest.method,
-    headers: {
-      'User-Agent': clientRequest.headers['user-agent'],
-    },
-  };
+// Serve static files from "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-  const serverRequest = parsedSSL.request(options, function (serverResponse) {
-    let body = '';
-    if (String(serverResponse.headers['content-type']).indexOf('text/html') !== -1) {
-      serverResponse.on('data', function (chunk) {
-        body += chunk;
-      });
-
-      serverResponse.on('end', function () {
-        // Make changes to HTML files when they're done being read.
-        // body = body.replace(`example`, `Cat!`);
-
-        clientResponse.writeHead(serverResponse.statusCode, serverResponse.headers);
-        clientResponse.end(body);
-      });
-    } else {
-      serverResponse.pipe(clientResponse, {
-        end: true,
-      });
-      clientResponse.contentType(serverResponse.headers['content-type']);
-    }
-  });
-
-  serverRequest.end();
+// Homepage with Proxy Form
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.listen(proxyServerPort);
-console.log(`Proxy server listening on port ${proxyServerPort}`);
+// Proxy Endpoint
+app.use('/proxy', (clientRequest, clientResponse) => {
+    const targetUrl = clientRequest.query.url;
+
+    if (!targetUrl) {
+        return clientResponse.status(400).send('Missing url parameter.');
+    }
+
+    // Validate URL
+    let parsedUrl;
+    try {
+        parsedUrl = new URL(targetUrl);
+    } catch (err) {
+        return clientResponse.status(400).send('Invalid URL.');
+    }
+
+    const parsedHost = parsedUrl.hostname;
+    let parsedPort;
+    let parsedSSL;
+    if (parsedUrl.protocol === 'https:') {
+        parsedPort = 443;
+        parsedSSL = https;
+    } else if (parsedUrl.protocol === 'http:') {
+        parsedPort = 80;
+        parsedSSL = http;
+    }
+
+    const options = {
+        hostname: parsedHost,
+        port: parsedPort,
+        path: parsedUrl.pathname + parsedUrl.search,
+        method: clientRequest.method,
+        headers: {
+            'User-Agent': clientRequest.headers['user-agent'],
+        },
+    };
+
+    const serverRequest = parsedSSL.request(options, function (serverResponse) {
+        let body = '';
+        if (String(serverResponse.headers['content-type']).indexOf('text/html') !== -1) {
+            serverResponse.on('data', function (chunk) {
+                body += chunk;
+            });
+
+            serverResponse.on('end', function () {
+                clientResponse.writeHead(serverResponse.statusCode, serverResponse.headers);
+                clientResponse.end(body);
+            });
+        } else {
+            serverResponse.pipe(clientResponse, {
+                end: true,
+            });
+            clientResponse.contentType(serverResponse.headers['content-type']);
+        }
+    });
+
+    serverRequest.on('error', (err) => {
+        console.error(err);
+        clientResponse.status(500).send('Proxy error.');
+    });
+
+    serverRequest.end();
+});
+
+// Start the Server
+app.listen(PORT, () => {
+    console.log(`Proxy server is running on port ${PORT}`);
+});
